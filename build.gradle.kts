3950803c8dff14d23930e5b9aee9fddc86fe5c6d
Lyzev <lyzev.info@gmail.com>
2022-04-06 00:05:44 +0200

    added missing maven plugin
    Signed-off-by: Lyzev <lyzev.info@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.6.20"
     id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("maven-publish")
     id("org.jetbrains.dokka") version "1.6.10"
 }
 
