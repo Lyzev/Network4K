6a11c35d1a21c37cfdef8c4591bf77cae1c88ac0
Lyzev <lyzev.info@gmail.com>
2022-04-06 00:01:26 +0200

    Added missing dependency
    Signed-off-by: Lyzev <lyzev.info@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,28 @@ import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.10"
+    kotlin("jvm") version "1.6.20"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
     id("org.jetbrains.dokka") version "1.6.10"
 }
 
 operator fun Project.get(property: String): String = property(property) as String
 
+buildscript {
+    repositories {
+        maven {
+            url = uri("https://plugins.gradle.org/m2/")
+        }
+    }
+    dependencies {
+        classpath("gradle.plugin.com.github.johnrengelman:shadow:7.1.2")
+    }
+}
+
+apply(plugin = "com.github.johnrengelman.shadow")
+
 group = "me.lyzev"
-version = "1.0"
+version = "1.1"
 
 repositories {
     mavenCentral()
